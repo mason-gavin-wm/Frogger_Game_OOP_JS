2e77af362870aef6d2bf28fe22c5e85ca79177cd
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-10-18 15:30:00 -0700

    starter some code for game

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -1,3 +1,5 @@
+
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -24,14 +26,38 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
 
+    this.x = 0;
+    this.y = 0;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.render = function() {
+
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+};
+
+Player.prototype.handleInput = function(key) {
+
+if(key == 'up'){
+    this.y -= 86;
+}
+};
+
+Player.prototype.update = function(){
+
+
+
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
-
+var allEnemies = [];
+var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
